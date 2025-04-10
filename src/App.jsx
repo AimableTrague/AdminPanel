53b018f6a4b323b2e8c5968424a84276490f8a87
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-10 07:12:31 +0200

    First commit of Admin Panel Web

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './index.css'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import TopBar from './scenes/Global/Topbar'
+import SideBar from './scenes/Global/Sidebar'
+import Dashboard from './scenes/Dashboard'
+import Team from './scenes/team/index'
+import Invoices from './scenes/invoices/index'
+import Contacts from './scenes/contacts/index'
+import Calendar from './scenes/calendar/index'
+import Form from './scenes/form/index'
+import FAQ from './scenes/FAQ/index'
+import Pie from './scenes/pie/index'
+import Bar from './scenes/bar/index'
+import Line from './scenes/line/index'
+import Geography from './scenes/geography'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  const [theme, colorMode] = useMode()
+
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SideBar />
+          <main className='content'>
+            <TopBar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/calendar' element={<Calendar />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/geography' element={<Geography />} />
+            </Routes>
+            </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
+  )
+}
+
+export default App
---
+++ src/scenes/FAQ/index.jsx
@@ -0,0 +1,94 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { tokens } from "../../theme";
+
+
+import React from 'react'
+
+const FAQ = () => {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An Important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illo aperiam
+                    error explicabo, obcaecati at quis voluptates et in est volupt
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Another Important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illo aperiam
+                    error explicabo, obcaecati at quis voluptates et in est volupt
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Your favorite question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illo aperiam
+                    error explicabo, obcaecati at quis voluptates et in est volupt
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Some random question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illo aperiam
+                    error explicabo, obcaecati at quis voluptates et in est volupt
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Another one
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illo aperiam
+                    error explicabo, obcaecati at quis voluptates et in est volupt
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+    </Box>
+  )
+}
+
+export default FAQ
