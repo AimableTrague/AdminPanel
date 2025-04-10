5efd811e9819cc70c3c88ea1eab0860f4ada2b7d
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-10 09:55:20 +0200

    updated the table headers

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -57,8 +57,8 @@ const Invoices = () => {
           },
           "& .name-column--cell": {
             color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
+          }, 
+          "& .MuiDataGrid-columnHeader": {
             backgroundColor: colors.blueAccent[700],
             borderBottom: "none",
           },
